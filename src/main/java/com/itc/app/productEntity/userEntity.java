b737950953432e0a256b35d26e08109c03206103
MagicEmperor <itsfaizzan.khan@gmail.com>
2024-12-14 16:34:52 +0530

    userNumber to unique

--- src/main/java/com/itc/app/productEntity/userEntity.java
+++ src/main/java/com/itc/app/productEntity/userEntity.java
@@ -10,25 +10,28 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "userTable")
 public class userEntity {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long userId;
 	@Column(name = "userName")
 	private String userName;
-	@Column(name ="userPassword")
+	@Column(name = "userPassword")
 	private String userPassword;
-	@Column(name = "userPhone")
+	@Column(name = "userPhone", unique = true)
 	private String userPhone;
-	@Column(name ="userLocation")
+	@Column(name = "userLocation")
 	private String userLocation;
 	@Column(name = "userRole")
 	private String userRole;
+
 	public userEntity() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public userEntity(long userId, String userName, String userPassword, String userPhone, String userLocation, String userRole) {
+
+	public userEntity(long userId, String userName, String userPassword, String userPhone, String userLocation,
+			String userRole) {
 		super();
 		this.userId = userId;
 		this.userName = userName;
@@ -37,42 +40,53 @@ public class userEntity {
 		this.userLocation = userLocation;
 		this.userRole = userRole;
 	}
+
 	public long getUserId() {
 		return userId;
 	}
+
 	public void setUserId(long userId) {
 		this.userId = userId;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getUserPassword() {
 		return userPassword;
 	}
+
 	public void setUserPassword(String userPassword) {
 		this.userPassword = userPassword;
 	}
+
 	public String getUserPhone() {
 		return userPhone;
 	}
+
 	public void setUserPhone(String userPhone) {
 		this.userPhone = userPhone;
 	}
+
 	public String getUserLocation() {
 		return userLocation;
 	}
+
 	public void setUserLocation(String userLocation) {
 		this.userLocation = userLocation;
 	}
+
 	public String getUserRole() {
 		return userRole;
 	}
+
 	public void setUserRole(String userRole) {
 		this.userRole = userRole;
 	}
-	
-	
+
 }
